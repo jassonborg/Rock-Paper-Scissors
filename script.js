4ca70af1dfa0046799762744ae0a949c20e3ad3a
jassonborg <jasson.borg@gmail.com>
2024-05-20 15:02:59 +1200

    Add User Choice function in script.js

--- script.js
+++ script.js
@@ -13,7 +13,8 @@ let choice;
 //if random equals to 0 = Rock
 if (random === 0) {
     choice = "Rock";
-} //if random equals to 1 = Paper
+} 
+//if random equals to 1 = Paper
 else if (random === 1) {
     choice = "Paper";
 } 
@@ -27,3 +28,27 @@ return choice;
 }
 
 
+//Create a function getHumanChoice()
+function getHumanChoice(){
+
+//Create a userInput variable to store the user prompt input
+let userInput = prompt("Enter Rock, Paper or Scissors and Hit Enter to Begin Game");
+
+//Create a validInput variable to store valid Values
+let validChoices = ["Rock", "Paper", "Scissors"];
+
+//Check if userInput and validChoices match
+if (validChoices.includes(userInput)){
+
+    //Return input
+    return userInput;
+    
+} else {
+
+    //Return error if not valid input try again
+    alert("Invalid Choice! Please enter valid Input of Rock, Paper or Scissors")
+
+    //Return getHumanChoice() function to run function again
+    return getHumanChoice();
+}
+}
